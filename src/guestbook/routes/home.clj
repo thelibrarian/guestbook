20c91fcc083fb02aff5a698cf0c71fe32565926b
Tony Kemp <tony@agrav.net>
2014-12-28 22:52:40 +1100

    Adds guests and messages.

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -2,16 +2,28 @@
   (:require [compojure.core :refer :all]
             [guestbook.views.layout :as layout]))
 
-(defn home []
+(defn home [& [name error message]]
   (layout/common
     [:h1 "Guestbook"]
     [:p "Welcome to my guestbook"]
+    [:p error]
+    (show-guests)
     [:hr]
     [:form
      [:p "Name:"]
      [:input]
      [:p "Message:"]
      [:textarea {:rows 10 :cols 40}]]))
+
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Hello" :name "Bob" :timestamp nil}
+          {:message "Foo" :name "Bar" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]])])
 
 (defroutes home-routes
   (GET "/" [] (home)))
