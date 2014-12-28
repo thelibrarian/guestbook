8c9bbf06e69f67c3de7688077e84ea3b064e56a2
Tony Kemp <tony@agrav.net>
2014-12-28 22:44:45 +1100

    Initial commit of Guestbook app for Web Dev with Clojure.

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject guestbook "0.1.0-SNAPSHOT"
+  :description "Guestbook Exercise from Web Development with Clojure"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [ring-server "0.3.1"]
+                 [lib-noir "0.9.5"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.xerial/sqlite-jdbc "3.8.7"]]
+  :plugins [[lein-ring "0.8.12"]]
+  :ring {:handler guestbook.handler/app
+         :init guestbook.handler/init
+         :destroy guestbook.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}}
+   :dev
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.3.1"]]}})
