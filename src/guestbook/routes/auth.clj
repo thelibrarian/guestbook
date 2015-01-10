62f42c112e28f501ef878a3db5a892152071d7be
Tony Kemp <tony@agrav.net>
2015-01-10 12:12:43 +1100

    Adds proper user registration.

--- src/guestbook/routes/auth.clj
+++ src/guestbook/routes/auth.clj
@@ -1,11 +1,13 @@
 (ns guestbook.routes.auth
   (:require [compojure.core :refer [defroutes GET POST]]
             [guestbook.views.layout :as layout]
+            [guestbook.models.db :as db]
             [hiccup.form :refer
              [form-to label text-field password-field submit-button]]
             [noir.response :refer [redirect]]
             [noir.session :as session]
-            [noir.validation :refer [rule errors? has-value? on-error]]))
+            [noir.validation :refer [rule errors? has-value? on-error]]
+            [noir.util.crypt :as crypt]))
 
 (defn format-error [[error]]
   [:p.error error])
@@ -46,12 +48,19 @@
       (session/put! :user id)
       (redirect "/"))))
 
+(defn handle-registration [id pass pass1]
+  (rule (= pass pass1)
+        [:pass "passwords did not match"])
+  (if (errors? :pass)
+    (registration-page)
+    (do
+      (db/add-user-record {:id id :pass (crypt/encrypt pass)})
+      (redirect "/login"))))
+
 (defroutes auth-routes
   (GET "/register" [_] (registration-page))
   (POST "/register" [id pass pass1]
-        (if (= pass pass1)
-          (redirect "/")
-          (registration-page)))
+        (handle-registration id pass pass1))
   (GET "/login" [_] (login-page))
   (POST "/login" [id pass]
         (handle-login id pass))
