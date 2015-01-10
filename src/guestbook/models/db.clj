95c36684c845fad3484e1229c69a3f6fda53ca68
Tony Kemp <tony@agrav.net>
2015-01-10 12:06:26 +1100

    Adds user account table to database.
    Including insert and get functions.

--- src/guestbook/models/db.clj
+++ src/guestbook/models/db.clj
@@ -16,6 +16,14 @@
      [:message "TEXT"])
     (sql/do-commands "CREATE INDEX timestamp_index ON guestbook (timestamp)")))
 
+(defn create-user-table []
+  (sql/with-connection db
+    (sql/create-table
+     :users
+     [:id "TEXT PRIMARY KEY"]
+     [:pass "TEXT"]
+     [:timestamp "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"])))
+
 (defn get-messages []
   (sql/with-connection db
     (sql/with-query-results res
@@ -28,3 +36,12 @@
      :guestbook
      [:name :message]
      [name message])))
+
+(defn add-user-record [user]
+  (sql/with-connection db
+    (sql/insert-record :users user)))
+
+(defn get-user [id]
+  (sql/with-connection db
+    (sql/with-query-results
+      res ["select * from users where id = ?" id] (first res))))
